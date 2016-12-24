6148a9cde3746154bcaa7a5e3b1ae618cee347e2
SharanSinivasagam <kumamaru93@gmail.com>
2016-12-24 18:23:39 +0800

    CarWorkshopSystem sprint 1 sharan
    Task 1st Sprint Completed

---
+++ CarWorkshopSystem sprint 1 sharan/Inventory/updateInventory.vb
@@ -0,0 +1,230 @@
+﻿Public Class updateInventory
+
+    Dim tempID As String
+    Dim tempName As String
+    Dim tempQuantity As Integer
+    Dim tempBatchNo As String
+    Dim tempCategory As String
+    Dim tempAD As Date
+    Dim tempCondition As String
+    Dim tempBrand As String
+    Dim tempDescription As String
+    Dim tempSpecification As String
+    Dim tempStatus As String
+    Dim tempSupplier As String
+    Dim tempPrice As Double
+    Dim tempImage As String
+    Dim tempPath As String
+
+    Dim categoryI As Integer
+    Dim conditionI As Integer
+    Dim supplierI As Integer
+
+    Dim picStore As String
+    Dim shortenPath As String
+    Dim fullPath As String
+
+    Private Sub loadImage()
+        If tempPath = "None" Then
+            imageBox.ImageLocation = ""
+        Else
+            imageBox.ImageLocation = tempPath
+            shortenPath = tempImage
+            fullPath = tempPath
+        End If
+    End Sub
+
+    Private Function checkEmptyBrand() As Boolean
+        Dim checkState As Boolean
+        If brandTxt.Text = "" Or brandTxt.Text.Trim().Length < 4 Then
+            checkState = False
+            verifyBrand.Text = "*At least 4 characters"
+        ElseIf brandTxt.TextLength >= 4 Then
+            verifyBrand.Text = ""
+            checkState = True
+        End If
+        Return checkState
+    End Function
+
+    Private Function checkEmptyDes() As Boolean
+        Dim checkState As Boolean
+        If descriptionBox.Text = "" Or descriptionBox.Text.Trim().Length < 4 Then
+            checkState = False
+            verifyDes.Text = "*At least 4 characters"
+        ElseIf descriptionBox.TextLength >= 4 Then
+            verifyDes.Text = ""
+            checkState = True
+        End If
+        Return checkState
+    End Function
+
+    Private Function checkEmptySpec() As Boolean
+        Dim checkState As Boolean
+        If specificationBox.Text = "" Or specificationBox.Text.Trim().Length < 4 Then
+            checkState = False
+            verifySpec.Text = "*At least 4 characters"
+        ElseIf specificationBox.TextLength >= 4 Then
+            verifySpec.Text = ""
+            checkState = True
+        End If
+        Return checkState
+    End Function
+
+    Private Sub resetInfo()
+        'Dim imgByte() As Byte = System.Text.Encoding.ASCII.GetBytes("0xFFD8FFE000104A46494600010100000100010000FFDB00840009060712121215121311161515171515151817161717151618171515161615151715181E2820181A251D151521312225292B2E2E2E171F3338332C37282D2E2B010A0A0A0E0D0E1B10101B2D251F252D2D2D2D2D2D2E2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D322D")
+        'System.Text.Encoding.ASCII.GetBytes(tempImage)
+
+        idLabel.Text = tempID
+        nameTxt.Text = tempName
+        quantityNum.Value = tempQuantity
+        batchTxt.Text = tempBatchNo
+
+        categoryI = categoryCombo.FindString(tempCategory)
+        categoryCombo.SelectedIndex = categoryI
+
+        dateText.Text = tempAD.ToString("dd/MM/yyyy")
+
+        conditionI = conditionCombo.FindString(tempCondition)
+        conditionCombo.SelectedIndex = conditionI
+
+        brandTxt.Text = tempBrand
+        descriptionBox.Text = tempDescription
+        specificationBox.Text = tempSpecification
+
+        If tempStatus = "Available" Then
+            availableRadio.Select()
+        Else
+            disabledRadio.Select()
+        End If
+
+        Dim db As New DBDataContext
+        Dim rs = From s In db.Suppliers
+                 Select s.supplierName
+
+        supplierCombo.DataSource = rs.ToArray()
+        supplierCombo.Enabled = True
+
+        supplierI = supplierCombo.FindString(tempSupplier)
+        supplierCombo.SelectedIndex = supplierI
+
+        priceNum.Value = CDec(tempPrice)
+        imageBox.ImageLocation = tempPath
+
+        'Verification Labels
+        verifyBrand.Text = ""
+        verifyDes.Text = ""
+        verifySpec.Text = ""
+
+    End Sub
+
+    Private Sub updateInventory_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tempID = Inventory.tempID
+        tempName = Inventory.tempName
+        tempQuantity = Inventory.tempQuantity
+        tempBatchNo = Inventory.tempBatchNo
+        tempCategory = Inventory.tempCategory
+        tempAD = Inventory.tempAD
+        tempCondition = Inventory.tempCondition
+        tempBrand = Inventory.tempBrand
+        tempDescription = Inventory.tempDescription
+        tempSpecification = Inventory.tempSpecification
+        tempStatus = Inventory.tempStatus
+        tempSupplier = Inventory.tempSupplier
+        tempPrice = Inventory.tempPrice
+        tempImage = Inventory.tempImage
+        tempPath = Inventory.tempPath
+
+        loadImage()
+        resetInfo()
+    End Sub
+
+
+    Private Sub updateBtn_Click(sender As Object, e As EventArgs) Handles updateBtn.Click
+        If checkEmptyBrand() = True And checkEmptyDes() = True And checkEmptySpec() = True Then
+            Try
+                Dim dbUpdate As New DBDataContext
+                Dim upDate As ItemStorage = dbUpdate.ItemStorages.FirstOrDefault(Function(o) o.inventoryID = tempID)
+
+                upDate.stockQuantity = CInt(quantityNum.Value)
+                upDate.stockCategory = categoryCombo.SelectedItem.ToString
+                upDate.stockBrand = brandTxt.Text
+                upDate.stockSupplier = supplierCombo.SelectedItem.ToString
+                upDate.stockPrice = priceNum.Value
+                upDate.stockDescription = descriptionBox.Text
+                upDate.stockSpecification = specificationBox.Text
+                upDate.stockCondition = conditionCombo.SelectedItem.ToString
+
+                If availableRadio.Checked = True Then
+                    upDate.stockStatus = "Available"
+                Else
+                    upDate.stockStatus = "Disabled"
+                End If
+
+                If picStore = "" And shortenPath = "" Then
+                    shortenPath = "None"
+                    fullPath = "None"
+                End If
+                upDate.stockImage = shortenPath
+                upDate.stockPath = fullPath
+
+                'quantity category brand supplier price descrip specific condition status
+
+                dbUpdate.SubmitChanges()
+
+                categoryI = categoryCombo.SelectedIndex
+                conditionI = conditionCombo.SelectedIndex
+                supplierI = supplierCombo.SelectedIndex
+
+                'Verification Labels
+                verifyBrand.Text = ""
+                verifyDes.Text = ""
+                verifySpec.Text = ""
+
+                MessageBox.Show("The record has been updated", "Record Updated", MessageBoxButtons.OK)
+            Catch ex As Exception
+                MessageBox.Show("Error")
+            End Try
+        End If
+    End Sub
+
+    Private Sub resetBtn_Click(sender As Object, e As EventArgs) Handles resetBtn.Click
+        Dim checkMsg As Integer
+        checkMsg = MessageBox.Show("Are you sure you wish to reset all input?", "Warning", MessageBoxButtons.YesNo)
+        If checkMsg = 6 Then
+            resetInfo()
+        End If
+    End Sub
+
+    Private Sub closeBtn_Click(sender As Object, e As EventArgs) Handles closeBtn.Click
+        Me.Close()
+    End Sub
+
+    Private Sub uploadBtn_Click(sender As Object, e As EventArgs) Handles uploadBtn.Click
+        Try
+            Dim dlg As New OpenFileDialog
+            dlg.Filter = "JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif|All Files (*.*)|*.*"
+            dlg.Title = "Select Product Image"
+            If dlg.ShowDialog = DialogResult.OK Then
+                picStore = dlg.FileName.ToString()
+                imageBox.ImageLocation = picStore
+                shortenPath = "../Images/Products/" + System.IO.Path.GetFileName(picStore)
+                fullPath = picStore
+            End If
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub descriptionBox_TextChanged(sender As Object, e As EventArgs) Handles descriptionBox.TextChanged
+        checkEmptyDes()
+    End Sub
+
+    Private Sub specificationBox_TextChanged(sender As Object, e As EventArgs) Handles specificationBox.TextChanged
+        checkEmptySpec()
+    End Sub
+
+    Private Sub brandTxt_TextChanged(sender As Object, e As EventArgs) Handles brandTxt.TextChanged
+        checkEmptyBrand()
+    End Sub
+End Class
---
+++ CarWorkshopSystem sprint 1 sharan/Staff/StaffUpdateForm.vb
@@ -0,0 +1,257 @@
+﻿Imports System.IO
+
+Public Class StaffUpdateForm
+    Public Shared SelectedId As String
+    Friend Shared selection As Integer = 0
+    Friend used As Integer = 0
+    Friend staffPicture As Image
+    Dim staPicName As String = ""
+    Dim clickPic As Integer = 0
+    Private Sub ResetForm()
+        txtStaffName.Text = ""
+        txtPassword.Text = ""
+        mskStaffIc.Text = ""
+        rbStaffMale.Checked = False
+        rbStaffFemale.Checked = False
+        cboStaffRace.Text = ""
+        txtStaffContact.Text = ""
+        txtstaffEmailAddress.Text = ""
+        cboPosition.Text = ""
+        txtStaffAddress.Text = ""
+        cboStaffStatus.Text = ""
+        lblGenderEmpty.Visible = False
+        lblPasswordEmpty.Visible = False
+        lblPositionEmpty.Visible = False
+        lblStaffAddressEmpty.Visible = False
+        lblStaffContact.Visible = False
+        lblStaffEmailFormatError.Visible = False
+        lblStaffNameEmpty.Visible = False
+        lblStaffRaceEmpty.Visible = False
+
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        ResetForm()
+        Me.Close()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Try
+            Dim err As Integer = 0
+            If rbStaffMale.Checked = False And rbStaffFemale.Checked = False Then
+                lblGenderEmpty.Visible = True
+                err += 1
+            End If
+            If txtPassword.Text = "" Then
+                lblPasswordEmpty.Visible = True
+                err += 1
+            End If
+            If cboPosition.Text = "" Then
+                lblPositionEmpty.Visible = True
+                err += 1
+            End If
+            If txtStaffAddress.Text = "" Then
+                lblStaffAddressEmpty.Visible = True
+                err += 1
+            End If
+            If txtStaffContact.Text = "" Then
+                lblStaffContact.Visible = True
+                err += 1
+            End If
+            If txtstaffEmailAddress.Text = "" Then
+                lblStaffEmailFormatError.Visible = True
+                err += 1
+            End If
+          
+            If txtStaffName.Text = "" Then
+                lblStaffNameEmpty.Visible = True
+                err += 1
+            End If
+            If cboStaffRace.Text = "" Then
+                lblStaffRaceEmpty.Visible = True
+                err += 1
+            End If
+        
+        Dim selectedValue As String = ""
+            Dim emailFormatCorrect As Integer = 0
+        Dim staffName As String = txtStaffName.Text
+        Dim staffIC As String = mskStaffIc.Text
+        Dim position = cboPosition.SelectedItem.ToString
+        If rbStaffMale.Checked Then
+            selectedValue = "Male"
+        ElseIf rbStaffFemale.Checked Then
+            selectedValue = "Female"
+
+        End If
+        Dim contact = txtStaffContact.Text
+        Dim password As String = txtPassword.Text
+
+        Dim race As String = cboStaffRace.SelectedItem.ToString
+            Dim email = txtstaffEmailAddress.Text
+            If IsValidEmailFormat(email) = False Then
+                lblStaffEmailFormatError.Visible = True
+                emailFormatCorrect = 1
+            Else
+                lblStaffEmailFormatError.Visible = False
+                emailFormatCorrect = 0
+            End If
+        Dim address = txtStaffAddress.Text
+        Dim status As String = ""
+        If used = 0 Then
+            status = cboStaffStatus.SelectedItem.ToString
+        End If
+
+
+
+        Dim db As New DBDataContext()
+        Dim s As Staff = db.Staffs.FirstOrDefault(Function(o) o.staffIC = SelectedId)
+
+        s.name = staffName.ToString
+        s.staffIC = staffIC.ToString
+        s.position = position.ToString
+        s.gender = selectedValue.ToString
+        s.contactNo = contact.ToString
+        s.password = password.ToString
+
+        s.address = address.ToString
+        s.race = race.ToString
+        s.email = email.ToString
+
+        If used = 0 Then
+            s.status = status.ToString
+        End If
+        If clickPic = 1 Then
+            s.staffPic = File.ReadAllBytes(staPicName)
+        End If
+        staPicName = ""
+        clickPic = 0
+        s.emplomentDate = Convert.ToDateTime(emplomentDateTimePicker.Value.Date.ToString("yyyy/MM/dd")).Date
+
+
+
+
+            If err > 0 Then
+
+            Else
+                If emailFormatCorrect = 0 Then
+                    db.SubmitChanges()
+                    MessageBox.Show("Staff name " & s.name & " and Staff IC " & s.staffIC & " has been updated.", "Update Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    Me.Close()
+                End If
+            End If
+
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub StaffUpdateForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            ResetForm()
+            If selection = 1 Then
+                lblStatus.Visible = False
+                cboStaffStatus.Visible = False
+                lblPosition.Visible = False
+                cboPosition.Visible = False
+                used = 1
+            Else
+                lblStatus.Visible = True
+                cboStaffStatus.Visible = True
+                lblPosition.Visible = True
+                cboPosition.Visible = True
+            End If
+            selection = 0
+            Dim db As New DBDataContext()
+
+            Dim s As Staff = db.Staffs.FirstOrDefault(Function(o) o.staffIC = SelectedId)
+
+            Dim imgByte As Byte()
+            If Not s.staffPic = Nothing Then
+                imgByte = CType(s.staffPic.ToArray, Byte())
+                Dim stream As New MemoryStream(imgByte)
+                staffPicture = Image.FromStream(stream)
+            Else
+                staffPicture = Nothing
+            End If
+
+
+            txtStaffName.Text = s.name.ToString
+            mskStaffIc.Text = s.staffIC.ToString
+            cboPosition.Text = s.position.ToString
+            If s.gender.Equals("Male") Then
+                rbStaffMale.Checked = True
+            ElseIf s.gender.Equals("Female") Then
+                rbStaffFemale.Checked = True
+
+            End If
+            txtStaffContact.Text = s.contactNo.ToString
+            txtPassword.Text = s.password.ToString
+            cboStaffStatus.Text = s.status.ToString
+            txtStaffAddress.Text = s.address.ToString
+            txtstaffEmailAddress.Text = s.email.ToString
+            cboStaffRace.Text = s.race.ToString
+            emplomentDateTimePicker.Value = s.emplomentDate
+            PictureBox1.Image = staffPicture
+        Catch ex As Exception
+            MessageBox.Show("Please select Record", "Unselect Record", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Me.Close()
+        End Try
+    End Sub
+
+    Private Sub btnStaffPic_Click(sender As Object, e As EventArgs) Handles btnStaffPic.Click
+      
+        Dim OpenFileDialog1 As New OpenFileDialog
+        OpenFileDialog1.Filter = "Picture Files (*)|*.bmp;*.gif;*.jpg"
+        If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            PictureBox1.Image = Image.FromFile(OpenFileDialog1.FileName)
+            staPicName = OpenFileDialog1.FileName
+            clickPic = 1
+        End If
+
+    End Sub
+    Function IsValidEmailFormat(ByVal s As String) As Boolean
+        Try
+            Dim a As New System.Net.Mail.MailAddress(s)
+        Catch
+            Return False
+        End Try
+        Return True
+    End Function
+
+    Private Sub txtStaffName_TextChanged(sender As Object, e As EventArgs) Handles txtStaffName.TextChanged
+        lblStaffNameEmpty.Visible = False
+    End Sub
+
+    Private Sub txtPassword_TextChanged(sender As Object, e As EventArgs) Handles txtPassword.TextChanged
+        lblPasswordEmpty.Visible = False
+    End Sub
+
+    Private Sub rbStaffMale_CheckedChanged(sender As Object, e As EventArgs) Handles rbStaffMale.CheckedChanged
+        lblGenderEmpty.Visible = False
+    End Sub
+
+    Private Sub rbStaffFemale_CheckedChanged(sender As Object, e As EventArgs) Handles rbStaffFemale.CheckedChanged
+        lblGenderEmpty.Visible = False
+    End Sub
+
+    Private Sub cboStaffRace_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboStaffRace.SelectedIndexChanged
+        lblStaffRaceEmpty.Visible = False
+    End Sub
+
+    Private Sub txtStaffContact_TextChanged(sender As Object, e As EventArgs)
+        lblStaffContact.Visible = False
+    End Sub
+
+    Private Sub txtstaffEmailAddress_TextChanged(sender As Object, e As EventArgs) Handles txtstaffEmailAddress.TextChanged
+        lblStaffEmailFormatError.Visible = False
+    End Sub
+
+    Private Sub cboPosition_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboPosition.SelectedIndexChanged
+        lblPositionEmpty.Visible = False
+    End Sub
+
+    Private Sub txtStaffAddress_TextChanged(sender As Object, e As EventArgs) Handles txtStaffAddress.TextChanged
+        lblStaffAddressEmpty.Visible = False
+    End Sub
+End Class
